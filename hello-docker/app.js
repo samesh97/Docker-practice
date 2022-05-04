123ca6bd743e02d677b08e9466e9e709e0b28f0a
samesh97 <Alahakoon123@>
2022-05-04 21:53:09 +0530

    Next commit

--- hello-docker/app.js
+++ hello-docker/app.js
@@ -1,1 +1,6 @@
-console.log("My First Docker App");
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => { res.send("Hello World"); });
+
+app.listen(4000, () => { console.log("Server is up and running on port 4000"); });
